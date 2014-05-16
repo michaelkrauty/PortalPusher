6f3057354f217d4bbaee78fb102a36f9594c6766
michaelkrauty <michaelkrauty@gmail.com>
2014-05-15 23:46:53 -0700

    Initial commit

---
+++ src/main/java/me/michaelkrauty/PortalPusher/Command/Command.java
@@ -0,0 +1,8 @@
+package me.michaelkrauty.PortalPusher.Command;
+
+public class Command {
+
+	public Command(String[] args) {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/main/java/me/michaelkrauty/PortalPusher/Command/Help.java
@@ -0,0 +1,12 @@
+package me.michaelkrauty.PortalPusher.Command;
+
+public class Help {
+
+	public Help() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Help(String[] args) {
+		// TODO Auto-generated constructor stub
+	}
+}

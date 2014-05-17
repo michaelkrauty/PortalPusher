b0f20b2ffa56d85b5a81279e053e1264b234c6f0
michaelkrauty <michaelkrauty@gmail.com>
2014-05-17 00:43:51 -0700

    update help

--- src/main/java/me/michaelkrauty/PortalPusher/Command/Help.java
+++ src/main/java/me/michaelkrauty/PortalPusher/Command/Help.java
@@ -6,6 +6,36 @@ public class Help {
 
 	public Help(Player player, String[] args) {
 		// TODO Auto-generated constructor stub
-		player.sendMessage(args[0]);
+		if (args.length == 0) {
+			for (int i = 0; i < getHelp(1).length; i++) {
+				player.sendMessage(getHelp(1)[i]);
+			}
+		} else {
+			try {
+				for (int i = 0; i < getHelp(1).length; i++) {
+					player.sendMessage(getHelp(Integer.parseInt(args[1]))[i]);
+				}
+			} catch (Exception e) {
+				for (int i = 0; i < getHelp(-1).length; i++) {
+					player.sendMessage(getHelp(-1)[i]);
+				}
+			}
+		}
+	}
+
+	private String[] getHelp(int page) {
+		if (page == 1) {
+			return new String[]{
+					"help line 1",
+					"help line 2",
+					"etc"
+			};
+		}
+		if (page > 1 || page == -1) {
+			return new String[]{
+					"Unknown help page!"
+			};
+		}
+		return null;
 	}
 }

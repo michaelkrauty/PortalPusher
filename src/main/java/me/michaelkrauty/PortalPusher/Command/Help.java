6a2661e51e72a5103969e44741cc0797d03507de
michaelkrauty <michaelkrauty@gmail.com>
2014-05-17 00:49:36 -0700

    update help

--- src/main/java/me/michaelkrauty/PortalPusher/Command/Help.java
+++ src/main/java/me/michaelkrauty/PortalPusher/Command/Help.java
@@ -6,19 +6,13 @@ public class Help {
 
 	public Help(Player player, String[] args) {
 		// TODO Auto-generated constructor stub
-		if (args.length == 0) {
-			for (int i = 0; i < getHelp(1).length; i++) {
-				player.sendMessage(getHelp(1)[i]);
+		try {
+			for (int i = 0; i < getHelp(Integer.parseInt(args[1])).length; i++) {
+				player.sendMessage(getHelp(Integer.parseInt(args[1]))[i]);
 			}
-		} else {
-			try {
-				for (int i = 0; i < getHelp(1).length; i++) {
-					player.sendMessage(getHelp(Integer.parseInt(args[1]))[i]);
-				}
-			} catch (Exception e) {
-				for (int i = 0; i < getHelp(-1).length; i++) {
-					player.sendMessage(getHelp(-1)[i]);
-				}
+		} catch (Exception e) {
+			for (int i = 0; i < getHelp(-1).length; i++) {
+				player.sendMessage(getHelp(-1)[i]);
 			}
 		}
 	}
